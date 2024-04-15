14d016605e06b96329a62695259504100c74e872
maxoldgood <maxoldgood@gmail.com>
2024-04-15 20:00:14 +0700

    feat: change result screen: show win numbers and match

--- src/app/components/number-button/number-button.jsx
+++ src/app/components/number-button/number-button.jsx
@@ -1,14 +1,16 @@
 import styles from './number-button.module.scss'
 
 export function NumberButton(props) {
-  const { number, handleButtonClick, isSelected, isDisabled } = props
+  const { number, handleButtonClick, isSelected, isDisabled, isWinNumber } = props
 
   return (
     <button
       disabled={isDisabled}
       onClick={handleButtonClick}
       value={number}
-      className={`${styles.number_button} ${isSelected && styles.number_button__selected}`}
+      className={`${styles.number_button} ${isWinNumber && !isSelected && styles.number_button__lose} ${
+        isSelected && styles.number_button__selected
+      } ${isSelected && isWinNumber && styles.number_button__selected_win}`}
     >
       {number}
     </button>
